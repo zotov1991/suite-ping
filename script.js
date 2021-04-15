8052b6889d62fae091b372d01aa4d5aab0908d42
zotov_r_g <zotov_r_g@mail.ru>
2021-04-16 00:30:52 +0300

    Inition
    Inition

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
-var blinking = true;
-var mypos = 0;
-var dpos = 25;
-var a = 1;
-var b = 1;
-stringm = new Array(4);
-       stringm[1] = "Эта кнопка";
-       stringm[2] = "изменила";
-       stringm[3] = "свою надпись";
-       stringm[4] = "Вам нравится?!";
-function ablink() {
-if (b==1) {
- document.forms[0].abutton.value = stringm[a]
-   b-- ;
-    a++;
-  if (a>4) {a = 1};}
-else {
- document.forms[0].abutton.value = "";
-   b++ }
+arrColor = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+function mouseOut() {
+  for (i = 0; i < 13; i++)
+    setTimeout ('document.blinkbutton.button.style.background = "#'+arrColor[15-i]+'0'+arrColor[15-i]+'FFF";', i * 50);
 }
-function myblink() {
- if (dpos > 13) {
-   mypos++}
- if (dpos > 48) {
-   mypos = 0;
-   dpos = 25;
-                 }
- else {dpos++}
-ianID = setTimeout ("myblink(); ablink()", 300);
+function mouseOver() {
+  for (i = 0; i < 15; i++)
+    setTimeout ('document.blinkbutton.button.style.background = "#'+arrColor[i]+'0'+arrColor[i]+'F31";', i * 50);
 }
-function stopit() {
-if (blinking==true) {
- clearTimeout(ianID);
-  document.forms[0].abutton.value = "Нажми для продолжения!";
-  blinking = false;
-}
-else {
-    myblink();
- document.forms[0].abutton.value = "Продолжаю анимацию" ;
- blinking = true;
-}
-}
+button.onclick = function() {
+  alert( 'Вот так это работает!' );
+  };
+  function ping(url){
+    new Image().src=url
+   }
